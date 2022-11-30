0a34b708b8393c9ef4a321193c22fd7deface8ed
y8nju <107169943+y8nju@users.noreply.github.com>
2022-11-30 15:54:54 +0900

    Design: header Search 영역 UI 추가

--- component/layout/header.tsx
+++ component/layout/header.tsx
@@ -1,5 +1,5 @@
 import {useState} from 'react'
-import { Box, Breakpoint, Button, Container, Toolbar, Typography } from "@mui/material";
+import { Box, Breakpoint, Button, Container, Grid, Paper, Toolbar, Typography } from "@mui/material";
 
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
@@ -8,6 +8,8 @@ import AnchorUserMenu from '../menu/anchorUserMenu';
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
+import { grey } from '@mui/material/colors';
+import SearchIcon from '@mui/icons-material/Search';
 
 interface Props {
 	sx?: any,
@@ -27,14 +29,26 @@ export default function Header (props: Props) {
 	return ( <Container maxWidth={false} disableGutters={true} sx={[{ borderBottom: 1, borderBottomColor: 'grey.300' }, props.sx]}>
 		<Container maxWidth={props.mw}>
 			<Toolbar>
-				<Link  href="/">
-					<img src="/images/logo.svg" alt="logo" width="102" height="32"
-						style={{verticalAlign: 'bottom'}} />
-				</Link>
-				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }} textAlign="center">
-					서치 영역 들어옴
-				</Typography>
-				<Box sx={{ flexGrow: 0 }}>
+				<Grid item>
+					<Link  href="/">
+						<img src="/images/logo.svg" alt="logo" width="102" height="32"
+							style={{verticalAlign: 'bottom'}} />
+					</Link>
+				</Grid>
+				<Grid  container flex={1} justifyContent="center" alignItems="center">
+					<Paper elevation={2} sx={[{display: 'flex', py: 0.5, px: 2, borderRadius: 6}, {'&:hover': {boxShadow: 3}}]}>
+						<Button color="info"
+							sx={{px:1.5, py: 0.5, lineHeight: 1.2, borderRadius:0,}}>어디든지</Button>
+						<Button color="info"
+							sx={{px:1.5, py: 0.5, lineHeight: 1.2, borderRadius:0, borderLeft: 1, borderRight: 1, borderColor: grey[300]}}>언제든 일주일</Button>
+						<Button color="info"
+							sx={{px:1.5, py: 0.5, lineHeight: 1.2, borderRadius:0,}}>게스트추가</Button>
+						<Box sx={{width: '32px', height: '32px', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '50%', backgroundColor: '#ff385c'}}>
+							<SearchIcon sx={{color: '#fff', fontSize: '16px'}} />
+						</Box>
+					</Paper>
+				</Grid>
+				<Grid item sx={{ flexGrow: 0 }}>
 					<Button variant="outlined" onClick={handleOpenUserMenu} color="inherit"
 						sx={{ color: 'grey.700',  borderColor: 'grey.300', borderRadius: 50, pr: 0.8, pl: 1.4 }}>
 						<MenuIcon fontSize='small' sx={{mr: 0.8}} />
@@ -46,7 +60,7 @@ export default function Header (props: Props) {
 						}
 					</Button>
 					<AnchorUserMenu active={anchorElUser} closeMenu={handleCloseUserMenu} setSignupOpen={setSignupOpen} />
-				</Box>
+				</Grid>
 			</Toolbar>
 		</Container>
 		<LoginAndSignUp open={Boolean(signupOpen)} onClose={setSignupOpen} closeMenu={handleCloseUserMenu}/>

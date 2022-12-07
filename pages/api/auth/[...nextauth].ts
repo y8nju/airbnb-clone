352957ace565acf515f42b62c24388f1bcddcdac
y8nju <107169943+y8nju@users.noreply.github.com>
2022-12-07 16:51:15 +0900

    Fixed: next auth credentcial login error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -19,44 +19,18 @@ export const authOption: NextAuthOptions = {
 			},
 			async authorize(credentials, req) {
 				let user : User;
-				user = {id : "iudjmi", email:"aru@aa.aa", name : "마스터"};
-				// return user;
-				 await dbConnect();
+				await dbConnect();
 				console.log('credentials', JSON.stringify(credentials));
-				//  if(!credentials) {
-				//  	throw new Error('not Credentials')
-				//  }}
 				try {
 					const got = await account.findOne({email: credentials!.email}); // id Find
 					if(!got )	{
 						return null;
 					}
 					if(compareSync(credentials!.password, got?.password)) {
-						return user = {id :  got._id.toString(), email : got.email, name : `${got.firstName}`};
+						return user = {id :  got._id.toString(), email : got.email, name : got.firstName};
 					}else {
 						return null;
 					}
-					
-					// if(getId) {
-					// 	console.log('getId', getId);
-					// 	const response = await fetch(process.env.NEXT_PUBLIC_SERVER_URI + '/api/account/signin',  {
-					// 		method: 'POST',
-					// 		headers: { 
-					// 			"Content-type": "application/json"
-					// 		},
-					// 		body: JSON.stringify({email: credentials.email, password: credentials.password})
-					// 	})
-					// 	console.log("authorize : ", response);
-					// 	const data = await response.json();
-
-					// 	if (response) {
-					// 		return { email: data.email,
-					// 			name: `${data.firstName}`,
-					// 		} as any;
-					// 	}
-					// }else {
-					// 	throw new Error('invalid email/password');
-					// }
 				} catch (e) {
 					console.log("SERVER - AUTHORIZE");
 					console.log(e);
